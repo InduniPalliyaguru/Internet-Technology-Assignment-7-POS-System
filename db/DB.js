2ff5bc350713d7ed1fece2f557bf6df9a338ae69
induni <lakmiindu11@gmail.com>
2026-04-22 19:14:42 +0530

    update DB

--- db/DB.js
+++ db/DB.js
@@ -11,6 +11,48 @@ export let customerDB = [
         customerAddress: "Galle",
         customerSalary: "100000.00",
     },
+    {
+        customerId: "C00-002",
+        customerName: "Lithira Jayanaka",
+        customerAddress: "Galle",
+        customerSalary: "100000.00",
+    },
+    {
+        customerId: "C00-003",
+        customerName: "Imassi Siyumi",
+        customerAddress: "Galle",
+        customerSalary: "100000.00",
+    },
+    {
+        customerId: "C00-004",
+        customerName: "Nimal Perera",
+        customerAddress: "Colombo",
+        customerSalary: "10000.00",
+    },
+    {
+        customerId: "C00-005",
+        customerName: "Sunil Fernand0",
+        customerAddress: "Jaffna",
+        customerSalary: "50000.00",
+    },
+    {
+        customerId: "C00-006",
+        customerName: "Amaya Kariyawasam",
+        customerAddress: "Matara",
+        customerSalary: "85000.00",
+    },
+    {
+        customerId: "C00-007",
+        customerName: "Fathima Safra",
+        customerAddress: "Gampaha",
+        customerSalary: "96000.00",
+    },
+    {
+        customerId: "C00-008",
+        customerName: "Saumya Perera",
+        customerAddress: "Negambo",
+        customerSalary: "45000.00",
+    },
 ];
 
 export let itemDB = [
@@ -20,17 +62,70 @@ export let itemDB = [
         itemQty: 50,
         unitPrice: "150.00",
     },
+    {
+        itemCode: "I00-002",
+        itemName: "Chocalate",
+        itemQty: 40,
+        unitPrice: "50.00",
+    },
+    {
+        itemCode: "I00-003",
+        itemName: "Toffee",
+        itemQty: 100,
+        unitPrice: "20.00",
+    },
+    {
+        itemCode: "I00-004",
+        itemName: "Soft Drink",
+        itemQty: 45,
+        unitPrice: "150.00",
+    },
+    {
+        itemCode: "I00-005",
+        itemName: "Biscuit",
+        itemQty: 80,
+        unitPrice: "180.00",
+    },
+    {
+        itemCode: "I00-006",
+        itemName: "Milo",
+        itemQty: 70,
+        unitPrice: "100.00",
+    },
+    {
+        itemCode: "I00-007",
+        itemName: "Yourget",
+        itemQty: 80,
+        unitPrice: "70.00",
+    },
+    {
+        itemCode: "I00-008",
+        itemName: "Tipi Tip",
+        itemQty: 50,
+        unitPrice: "120.00",
+    },
 ];
 
 export let ordersDB = [
-    {
-        orderId: "OID-001",
-        orderDate: "2025-03-24",
-        customerId: "C00-001",
-        discount: 5,
-        totalPrice: "1800.00",
-        orderDetails: [
-            { itemCode: "I00-001", unitPrice: 1200.00, qty: 5 },
-        ]
-    },
+    // {
+    //     orderId: "OID-001",
+    //     orderDate: "2025-03-24",
+    //     customerId: "C00-001",
+    //     discount: 5,
+    //     totalPrice: "1800.00",
+    //     orderDetails: [
+    //         { itemCode: "I00-001", unitPrice: 1200.00, qty: 5 },
+    //     ]
+    // },
+    // {
+    //     orderId: "OID-002",
+    //     orderDate: "2025-03-29",
+    //     customerId: "C00-005",
+    //     discount: 0,
+    //     totalPrice: "260.00",
+    //     orderDetails: [
+    //         { itemCode: "I00-008", unitPrice: 120.00, qty: 1 },
+    //         { itemCode: "I00-007", unitPrice: 70.00, qty: 2 }
+    //     ]
+    // },
 ];
